f1a067005ac0601602181efe371cd45aab5b7a20
sHalnes <svetlana.halnes@outlook.com>
2017-02-25 18:09:50 +0100

    debug = true

--- ChargeEasy/settings.py
+++ ChargeEasy/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "l4by4&p=c3_yb+zmf2z)!ot_=#q=3kkck#4a5gxb)ez1lx!ub)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False#True
 
 # Application definition
 
