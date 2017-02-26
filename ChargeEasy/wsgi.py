a69e58fd5b105d6c302b0643c242c564a015adc8
sHalnes <svetlana.halnes@outlook.com>
2017-02-26 20:42:04 +0100

    witenoise

--- ChargeEasy/wsgi.py
+++ ChargeEasy/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+#import os
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChargeEasy.settings")
+#application = get_wsgi_application()
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChargeEasy.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
