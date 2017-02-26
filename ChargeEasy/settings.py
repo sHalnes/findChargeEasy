dfe2383105ea0266c957ca154dd97d37a6976658
sHalnes <svetlana.halnes@outlook.com>
2017-02-26 20:33:56 +0100

    witenoise

--- ChargeEasy/settings.py
+++ ChargeEasy/settings.py
@@ -138,8 +138,8 @@ ALLOWED_HOSTS = ['.herokuapp.com',
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
